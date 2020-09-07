8dbc0a26230a98da0082775ac9b0c18c5958bdbb
green142f <41095204+green142f@users.noreply.github.com>
2020-09-07 10:17:23 -0700

    minor update
    Removed several print statements and the CIFAR100 class

--- cifar.py
+++ cifar.py
@@ -24,7 +24,6 @@ class CIFAR10(VisionDataset):
             downloaded again.
     """
     base_folder = 'Data'
-    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
     filename = "cifar-10-python.tar.gz"
     tgz_md5 = 'c58f30108f718f92721af3b95e74349a'
 
@@ -169,24 +168,3 @@ class CIFAR10(VisionDataset):
     def extra_repr(self):
         return "Split: {}".format("Train" if self.train is True else "Test")
 
-
-class CIFAR100(CIFAR10):
-    """`CIFAR100 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
-    This is a subclass of the `CIFAR10` Dataset.
-    """
-    base_folder = 'cifar-100-python'
-    url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
-    filename = "cifar-100-python.tar.gz"
-    tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
-    train_list = [
-        ['train', '16019d7e3df5f24257cddd939b257f8d'],
-    ]
-
-    test_list = [
-        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
-    ]
-    meta = {
-        'filename': 'meta',
-        'key': 'fine_label_names',
-        'md5': '7973b15100ade9c7d40fb424638fde48',
-    }
